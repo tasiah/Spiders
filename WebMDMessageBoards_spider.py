2749d9a8ae9ff07da3717bf24eafe9d6a2e2bbba
Anastasia Halim <tasiahalim@outlook.com>
2017-12-08 21:30:47 -0800

    Update yield
    now iterates through every page of every topic, yield includes topic

--- WebMDMessageBoards_spider.py
+++ WebMDMessageBoards_spider.py
@@ -3,7 +3,6 @@
 
 import scrapy
 
-#TODO: topics dict, utf encoding (still) It/'s
 class BurpeeSpider(scrapy.Spider):
     name = "webmdMB"
     rate = 1.
@@ -20,40 +19,39 @@ class BurpeeSpider(scrapy.Spider):
             )
 
     def parse(self, response):
-        topics = dict()
-        topic = response.css('div.browse-forums a.internal-link::text').extract()
         for href in response.css('div.browse-forums a.internal-link::attr(href)').extract():
             yield scrapy.Request(href, callback=self.parse_topic)
 
-        
-
+    # parse all the threads in every page of the topic (10 threads per page)
     def parse_topic(self, response):
         for href in response.css('li.content-item div.thread-detail a.internal-link::attr(href)').extract():
             yield scrapy.Request(href, callback=self.parse_thread)
 
-    # Method for parsing a product page
+        next_page = response.css('div.pager a.next::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse_topic)
+
     def parse_thread(self, response):
         
-        # extracting thread-body returns a list of each sentence, so concat them into
-        # a single string/paragraph
-        # also, extra spaces at beginning/end of paragraphs that I'm not sure how to get rid of
-        # without sacrificing a space in between sentences
         body = ""
         for text in response.css('ul.webmd-mb-thrd div.thread-body::text').extract():
-            body += text.strip() + " "
+            body += text.encode('utf-8').decode('utf-8').strip() + " "
 
         # TODO: nest responses?
         responses = []
         for r in response.css('ul.webmd-mb-rsp div.thread-detail'):
             rspBody = ""
             for text in r.css('div.thread-body::text').extract():
-                rspBody += text.strip() + " "
-            responses.append(rspBody.decode('utf-8'))
+                rspBody += text.encode('utf-8').decode('utf-8').strip() + " "
+            responses.append(rspBody)
 
         yield {
-            # to keep or not to keep username
+            'Topic': response.css('h1.title a::text').extract_first(),
             'Title': response.css('div.thread-detail a.internal-link::text').extract_first(),
             'Body': body,
             'Tags': response.css('li.tag-item a.tag::text').extract(),
             'Responses': responses
         }
+
+
